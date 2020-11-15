65f72a483ea42e1b5cfc9d0c2ce0c2babe4c56e9
Daniel Yankiver <dyankiver@gmail.com>
2020-11-15 16:56:05 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -13,7 +13,7 @@ def apply_coupons(cart, coupons)
       else
         cart_item_with_coupon = {
           :item => couponed_item_name,
-          :price => coupons[counter][:price]
+          :price => coupons[counter][:cost]
         }
       end 
     end
