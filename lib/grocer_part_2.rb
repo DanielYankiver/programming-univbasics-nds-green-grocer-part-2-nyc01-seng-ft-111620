9340a2bfe28713c08f5417fcffb85ff40d83b8a2
Daniel Yankiver <dyankiver@gmail.com>
2020-11-15 16:42:21 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -3,7 +3,7 @@ require_relative './part_1_solution.rb'
 def apply_coupons(cart, coupons)
   counter = 0 
   while counter < coupons.length
-    cart_item = find_item_by_name_in_collection
+    cart_item = find_item_by_name_in_collection(coupons[])
     counter += 1 
   end 
 end
