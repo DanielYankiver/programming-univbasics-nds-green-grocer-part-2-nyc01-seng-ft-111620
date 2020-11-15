862026c32cb2fe831ef724b2c154c97912c4233d
Daniel Yankiver <dyankiver@gmail.com>
2020-11-15 17:10:55 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -30,7 +30,7 @@ def apply_clearance(cart)
   counter = 0
   while counter < counter.length
   if cart[counter][:clearance]
-    cart[counter][:price] = cart[counter][:price] = (cart[counter][:price] )
+    cart[counter][:price] = cart[counter][:price] = (cart[counter][:price] * 0.20)
   end 
   counter += 1 
   end 
